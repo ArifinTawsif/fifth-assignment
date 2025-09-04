5ea04f92ec82a7854fe2c7dc195fdee098e58e59
ArifinTawsif <arifintawsif18@gmail.com>
2025-09-05 04:25:48 +0600

    added calling alert option

--- styles/styles.js
+++ styles/styles.js
@@ -1,48 +1,92 @@
 // heart count
 let count = 0;
 let heartCount = document.getElementById("heart-counter");
+
 // national service
 document.getElementById("national-btn").addEventListener("click", function () {
     count++;
     heartCount.textContent = count;
 });
+document.getElementById("national-call-btn").addEventListener("click", function () {
+  alert("You Are Calling The National Emergency Number! The Number You Have Dialed Is 999");
+});
+
+
 // police service 
 document.getElementById("police-btn").addEventListener("click", function () {
     count++;
     heartCount.textContent = count;
 });
+document.getElementById("police-call-btn").addEventListener("click", function () {
+  alert("You Are Calling The Police Station! The Number You Have Dialed Is 999");
+});
+
+
 // fire service 
 document.getElementById("fire-btn").addEventListener("click", function () {
     count++;
     heartCount.textContent = count;
 });
+document.getElementById("fire-call-btn").addEventListener("click", function () {
+  alert("You Are Calling The Fire Service Department! The Number You Have Dialed Is 999");
+});
+
+
 // ambulance service 
 document.getElementById("ambulance-btn").addEventListener("click", function () {
     count++;
     heartCount.textContent = count;
 });
+document.getElementById("ambu-call-btn").addEventListener("click", function () {
+  alert("You Are Calling The Ambulance Service! The Number You Have Dialed Is 1994-999999");
+});
+
+
 // women and child helpline
 document.getElementById("woman-child-btn").addEventListener("click", function () {
     count++;
     heartCount.textContent = count;
 });
+document.getElementById("womenChild-call-btn").addEventListener("click", function () {
+  alert("You Are Calling The Women And Children Safety Helpline! The Number You Have Dialed Is 109");
+});
+
+
 // anti corruption service 
 document.getElementById("anti-btn").addEventListener("click", function () {
     count++;
     heartCount.textContent = count;
 });
+document.getElementById("anti-call-btn").addEventListener("click", function () {
+  alert("You Are Calling Anti Corruption Helpline! The Number You Have Dialed Is 106");
+});
+
+
 // electricity service 
 document.getElementById("electric-btn").addEventListener("click", function () {
     count++;
     heartCount.textContent = count;
 });
+document.getElementById("electric-call-btn").addEventListener("click", function () {
+  alert("You Are Calling Electricity Service Helpline! The Number You Have Dialed Is 162616");
+});
+
+
 // brac service 
 document.getElementById("brac-btn").addEventListener("click", function () {
     count++;
     heartCount.textContent = count;
 });
+document.getElementById("brac-call-btn").addEventListener("click", function () {
+  alert("You Are Calling Brac Helpline! The Number You Have Dialed Is 16445");
+});
+
+
 // railway service
 document.getElementById("railway-btn").addEventListener("click", function () {
     count++;
     heartCount.textContent = count;
+});
+document.getElementById("railway-call-btn").addEventListener("click", function () {
+  alert("You Are Calling Bangladesh Railway Helpline! The Number You Have Dialed Is 163");
 });
