97d61d51426250c7618390597e2755ccff099649
ArifinTawsif <arifintawsif18@gmail.com>
2025-09-05 04:07:17 +0600

    added heart counter JS

---
+++ styles/styles.js
@@ -0,0 +1,48 @@
+// heart count
+let count = 0;
+let heartCount = document.getElementById("heart-counter");
+// national service
+document.getElementById("national-btn").addEventListener("click", function () {
+    count++;
+    heartCount.textContent = count;
+});
+// police service 
+document.getElementById("police-btn").addEventListener("click", function () {
+    count++;
+    heartCount.textContent = count;
+});
+// fire service 
+document.getElementById("fire-btn").addEventListener("click", function () {
+    count++;
+    heartCount.textContent = count;
+});
+// ambulance service 
+document.getElementById("ambulance-btn").addEventListener("click", function () {
+    count++;
+    heartCount.textContent = count;
+});
+// women and child helpline
+document.getElementById("woman-child-btn").addEventListener("click", function () {
+    count++;
+    heartCount.textContent = count;
+});
+// anti corruption service 
+document.getElementById("anti-btn").addEventListener("click", function () {
+    count++;
+    heartCount.textContent = count;
+});
+// electricity service 
+document.getElementById("electric-btn").addEventListener("click", function () {
+    count++;
+    heartCount.textContent = count;
+});
+// brac service 
+document.getElementById("brac-btn").addEventListener("click", function () {
+    count++;
+    heartCount.textContent = count;
+});
+// railway service
+document.getElementById("railway-btn").addEventListener("click", function () {
+    count++;
+    heartCount.textContent = count;
+});
